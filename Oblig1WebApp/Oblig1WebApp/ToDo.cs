a99bd61310e674be795c030662402d5652839d50
Felix Nilsen <felix1998nilsen@hotmail.com>
2019-10-21 13:55:26 +0200

    endringer i ToDo og endring av databasenavn

--- Oblig1WebApp/Oblig1WebApp/ToDo.cs
+++ Oblig1WebApp/Oblig1WebApp/ToDo.cs
@@ -8,24 +8,20 @@ namespace Oblig1WebApp
     public class ToDo
     {
         //Spørsmål
-        // - Skal vi kunne endre brukernavn/slette brukere?
-        // - Validering av passord og brukernavn, evt server og klient??
-        // - Nødvendig å sjekke om bruker med samme navn allerede er registrert?
+        // - Skal vi kunne endre brukernavn/slette brukere? Legge til mere funksjonalitet som å liste alle brukere, slette bruker, endre bruker (?)
+        // - Sikkerhet ved innlogging? SQL-injection?
+        // - Validering av passord og brukernavn, evt server og klient?
 
-        // - Fikse administrasjon av strekninger
-        // - Fikse administrasjon av avganger
-        // - Fikse administrasjon av priser
+        // - Fikse administrasjon av strekninger, priser og avganger
         // - Fikse administrasjon av annet som trengs
+        // ^ Dette er vel ferdig
 
         // - Logging av endringer til database
         // - Logging av feilsituasjoner til fil, bla feil som oppstår når databasen aksesseres
 
-        // - Slette visAvganger i connectionstringen i web.config (?)
         // - Validering av innlogging, registrering etc
         // - Skrive ut til bruker at registrering av ny bruker var suksessfult (Bruke session?)
         // - Når bruker logger seg ut, skrive ut at utlogging har blitt gjort (Sjekke session med javascript)
-        // - Legge til mere funksjonalitet som å liste alle brukere, slette bruker, endre bruker (?)
-        // - Før vi leverer: Endre navn på databasene i web-config
 
         // https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/preventing-cross-site-request-forgery-csrf-attacks
     }
